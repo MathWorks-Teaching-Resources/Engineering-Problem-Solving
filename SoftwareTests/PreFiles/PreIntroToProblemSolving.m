ce832bd442c4bdd7a4c563e49e0bfa73a00c7c23
callie <callie@mathworks.com>
2025-03-14 10:00:30 -0500

    Initial push of first release version of systems engineering.

---
+++ SoftwareTests/PreFiles/PreIntroToProblemSolving.m
@@ -0,0 +1,8 @@
+%  Pre-run script for IntroToProblemSolving.mlx
+% ---- Known Issues     -----
+KnownIssuesID = "";
+% ---- Pre-run commands -----
+ 
+out1.logsout{1}.Values.Data = zeros(51,1);
+out1.logsout{1}.Values.Time = (0:1:50)';
+% simData = zeros(50,1);
