5ceca5e099ec704bca8cce34be7e472b8dc8ef17
callie <callie@mathworks.com>
2025-07-03 13:52:26 -0500

    Added pre-files and update yml file to pass tests.

---
+++ SoftwareTests/PreFiles/PreDefiningRequirements.m
@@ -0,0 +1,6 @@
+%  Pre-run script for DefiningRequirements.mlx
+% ---- Known Issues     -----
+KnownIssuesID = "";
+% ---- Pre-run commands -----
+ 
+sltest.testmanager = @(x) disp("... Opening "+x);
