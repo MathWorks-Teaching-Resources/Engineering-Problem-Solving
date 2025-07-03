eb3675da918251a29d4f77e692de371b160923d6
callie <callie@mathworks.com>
2025-07-03 16:02:45 -0500

    Pre files update for testing

--- SoftwareTests/PreFiles/PreDefiningRequirementsSoln.m
+++ SoftwareTests/PreFiles/PreDefiningRequirementsSoln.m
@@ -2,7 +2,10 @@
 % ---- Known Issues     -----
 KnownIssuesID = "";
 % ---- Pre-run commands -----
- 
+
+sltest.testmanager.view = @(x) disp("... Opening "+x);
+sltest.testmanager.load = @(x) disp("... Opening "+x);
+sltest.testmanager.run = @(x) disp("... Opening "+x);
 sltest.testmanager.clear = @(x) disp("... Opening "+x);
 sltest.testmanager.clearResults = @(x) disp("... Opening "+x);
 sltest.testmanager.close = @(x) disp("... Opening "+x);
