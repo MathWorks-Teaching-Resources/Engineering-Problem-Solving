dc3ef2cb04f44949a6ff6856cc937d2a8994a3d7
callie <callie@mathworks.com>
2025-07-03 14:31:04 -0500

    add pre files for solution scripts

---
+++ SoftwareTests/PreFiles/PreDefiningRequirementsSoln.m
@@ -0,0 +1,6 @@
+%  Pre-run script for DefiningRequirementsSoln.mlx
+% ---- Known Issues     -----
+KnownIssuesID = "";
+% ---- Pre-run commands -----
+ 
+sltest.testmanager = @(x) disp("... Opening "+x);
---
+++ SoftwareTests/PreFiles/PreSystemsDecompositionSoln.m
@@ -0,0 +1,12 @@
+%  Pre-run script for SystemsDecompositionSoln.mlx
+% ---- Known Issues     -----
+KnownIssuesID = "";
+% ---- Pre-run commands -----
+ 
+web = @(x) disp("... Opening "+x);
+sltest.testmanager.view = @(x) disp("... Opening "+x);
+sltest.testmanager.load = @(x) disp("... Opening "+x);
+sltest.testmanager.run = @(x) disp("... Opening "+x);
+sltest.testmanager.clear = @(x) disp("... Opening "+x);
+sltest.testmanager.clearResults = @(x) disp("... Opening "+x);
+sltest.testmanager.close = @(x) disp("... Opening "+x);
